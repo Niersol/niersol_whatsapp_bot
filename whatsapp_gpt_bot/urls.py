c317665814ebe8a6210fc22ad48101b6a498392a
Sheikh <anasusman0324@gmail.com>
2025-01-25 18:09:59 +0500

    Initial Django App

---
+++ whatsapp_gpt_bot/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from bot.views import whatsapp_webhook
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('webhook/', whatsapp_webhook, name='whatsapp_webhook'),
+]
