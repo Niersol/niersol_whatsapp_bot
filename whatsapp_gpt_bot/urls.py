94070b879a20871f25c7b8fa8938adca78511ff9
Sheikh <anasusman0324@gmail.com>
2025-01-25 19:38:20 +0500

    fixed

--- whatsapp_gpt_bot/urls.py
+++ whatsapp_gpt_bot/urls.py
@@ -5,8 +5,5 @@ from bot.views import whatsapp_webhook
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('webhook', whatsapp_webhook, name='whatsapp_webhook'),
-<<<<<<< HEAD
-=======
     path('',include('bot.urls'))
->>>>>>> 123dc62 (updated privacy policy)
 ]
